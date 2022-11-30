e04d8c01129d4e5d9813f2054fdd8cf565c5f360
liming <1002464056@qq.com>
2022-11-30 22:10:07 +0800

    修改透明度

--- lib/widget/drawing_board.dart
+++ lib/widget/drawing_board.dart
@@ -200,6 +200,33 @@ class SignaturePainter extends CustomPainter {
       final ui.Rect b1 = Rect.fromCenter(center: center.translate(-halfSize, -halfSize),width: size,height: size);
       //0,0
       canvas.drawRect(b1, paint);
+      paint.color = Colors.white.withOpacity(1);
+      //0,1
+      canvas.drawRect(b1.translate(0, size), paint);
+      paint.color = Colors.black;
+      //0,2
+      canvas.drawRect(b1.translate(0, size*2), paint);
+      paint.color = Colors.black;
+      //1,0
+      canvas.drawRect(b1.translate(size, 0), paint);
+      paint.color = Colors.black;
+      //1,1
+      canvas.drawRect(b1.translate(size, size), paint);
+      paint.color = Colors.black;
+      //1,2
+      canvas.drawRect(b1.translate(size, size*2), paint);
+      paint.color =  Colors.white.withOpacity(1);
+      //2,0
+      canvas.drawRect(b1.translate(size*2, 0), paint);
+      paint.color = Colors.black;
+      //2,1
+      canvas.drawRect(b1.translate(size*2, size), paint);
+      paint.color = Colors.black;
+      //2,2
+      canvas.drawRect(b1.translate(size*2, size*2), paint);
+      /*
+      //0,0
+      canvas.drawRect(b1, paint);
       paint.color = Colors.grey.withOpacity(1);
       //0,1
       canvas.drawRect(b1.translate(0, size), paint);
@@ -224,6 +251,8 @@ class SignaturePainter extends CustomPainter {
       paint.color = Colors.black26;
       //2,2
       canvas.drawRect(b1.translate(size*2, size*2), paint);
+
+       */
 
     }
 
